36a0fb87d29b8aec23ef8f0ec7dac33ca4281beb
ShubhamBhoite7 <shubhambhoite.skn.it@gmail.com>
2025-10-13 16:38:56 +0530

    Setup Express app and MongoDB connection on port 8082 - Add Task model schema - Implement service layer for CRUD (create, read, update, delete) - Add configuration files (.env.example, gitignore)

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const taskModel = require("./models/task.model");
+
+const MONGODB_URI = "mongodb://localhost:27017/task-manager";
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ backend/models/task.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      status: { type: String, enum: ["TODO", "DONE"], default: "TODO" },
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const taskModel = mongoose.model("Task", taskSchema);
+
+module.exports = taskModel;
+console.log(taskModel);
